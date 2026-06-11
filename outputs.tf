b249fcc32f9fad715c3a5b36e11bf9120f5b785d
TigranAHarutyunyan <tigran.harutunyan.02@gmail.com>
2026-06-11 19:28:30 +0400

    task 4 Done correct structure

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "vm_public_ip" {
-  value       = azurerm_public_ip.pip.ip_address
-  description = "Virtual Machine Public IP"
-}
-output "vm_fqdn" {
-  description = "Virtual Machine FQDN"
-  value       = azurerm_public_ip.pip.fqdn
-}
